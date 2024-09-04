4acda74887b0b240ebbd5bc97d81b4e4abd8418a
Italo Rossy <117784579+ItaloRossySantiago@users.noreply.github.com>
2024-09-04 20:10:33 -0300

    Inclusão do primeiro app simples criado com storyboard.

--- ProjetoAlcooleGasolina/ViewController.swift
+++ ProjetoAlcooleGasolina/ViewController.swift
@@ -9,6 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var precoDaGasolina: UITextField!
+    @IBOutlet weak var precoDoAlcool: UITextField!
+    
+    @IBOutlet weak var resultadoTf: UILabel!
+    
+    @IBAction func calcular(_ sender: Any) {
+        if let precoDaGasolina = precoDaGasolina.text {
+            if let precoDoAlcool = precoDoAlcool.text {
+              let validarCampos = validarCampos(precoAlcool: precoDoAlcool, precoGasolina: precoDaGasolina)
+                if validarCampos {
+                    calcularMelhorPreco(precoAlcool: precoDoAlcool,precoGasolina: precoDaGasolina)
+                }
+                
+            }
+        }
+        
+        
+        
+    }
+    
+    func calcularMelhorPreco(precoAlcool:String, precoGasolina:String) {
+        if let Alcool = Double(precoAlcool) {
+            if let Gasolina = Double(precoGasolina) {
+                let calculo =  Alcool / Gasolina
+                
+                if calculo >= 0.7 {
+                    self.resultadoTf.text = "É melhor utilizar a gasolina"
+                } else {
+                    self.resultadoTf.text = "É melhor utilizar o alcool"
+                }
+            }
+        }
+        
+    }
+    
+    func validarCampos(precoAlcool:String, precoGasolina:String) -> Bool {
+        
+        if (precoAlcool.isEmpty) && (precoGasolina.isEmpty) {
+            return false
+        } else {
+            return true
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
